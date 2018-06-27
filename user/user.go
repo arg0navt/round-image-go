39143d1d280335a5be41530bc7bcc9eac0ce0543
argonavt11 <argonavt.al@yandex.ru>
2018-06-27 23:42:17 +0300

    db

--- user/user.go
+++ user/user.go
@@ -1,8 +1,16 @@
 package user
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
-func GetUser(c *gin.Context) {
+type User struct {
+    Email string
+}
+
+func GetAll(c *gin.Context) {
+	fmt.Println(c)
 	c.JSON(200, gin.H{
 		"user": 0,
 	})
