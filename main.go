1c78b6f57897cce3dabadefa04d6ef8215ce7b80
argonavt11 <argonavt.al@yandex.ru>
2018-07-09 21:15:33 +0300

    users

--- main.go
+++ main.go
@@ -4,37 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 	"./user"
 	"./mongo"
-	"net/http"
-	"log"
-	"github.com/PuerkitoBio/goquery"
 )
 
-type FindUser struct {
-	Name string
-	UrlProf string
-	Avatar string
-}
+
 
 func startPage(c *gin.Context) {
-	res, err := http.Get("https://kanobu.ru/shouts/all")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	var arrayUser []FindUser
-	doc.Find(".page-shouts .shout-item").Each(func(i int, s *goquery.Selection) {
-		newUser := FindUser{Name: s.Find(".shout-author--name").Text()}
-		arrayUser = append(arrayUser, newUser)
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-	}
 	c.JSON(200, gin.H{
-		"connect": arrayUser,
+		"connect": 1,
 	})
 }
 
@@ -45,7 +21,7 @@ func main() {
 	}
   	defer session.Close()
 	route := gin.Default()
-	v1 := route.Group("/user")
+	v1 := route.Group("/users")
 	route.GET("/", startPage)
 	{
 		v1.GET("/", user.GetAll)
--- user/user.go
+++ user/user.go
@@ -2,28 +2,40 @@ package user
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
+	"github.com/PuerkitoBio/goquery"
+	"net/http"
 	"fmt"
-	"../mongo"
 )
 
-//type CollectionUser struct {
-//	db *mgo.Collection
-//}
-//
-//type User struct {
-//	Id bson.ObjectId `bson:"_id"`
-//	Email string `bson:"email"`
-//}
-//
-//func GetCollectionUser(db *mgo.Database) {
-//	collection = &CollectionUser{}
-//}
+type FindUser struct {
+	Name, UrlProf, Avatar string
+}
 
 func GetAll(c *gin.Context) {
-	//usersList := []User{}
-	//db.Find(bson.M{}).All(&usersList)
-	fmt.Println(mongo.ConnectDb{})
+	res, err := http.Get("https://kanobu.ru/shouts/all")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	var arrayUser []FindUser
+	doc.Find(".page-shouts .shout-item").Each(func(i int, s *goquery.Selection) {
+		link, errLink := s.Find(".shout-author").Attr("href")
+		img, errImg := s.Find(".shout-item--pic").Attr("src")
+		if (errLink == true) && (errImg == true) {
+			newUser := FindUser{Name: s.Find(".shout-author--name").Text(), UrlProf: link, Avatar: img}
+			arrayUser = append(arrayUser, newUser)
+		}
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+	fmt.Println(arrayUser)
 	c.JSON(200, gin.H{
-		"users": 0,
+		"users": arrayUser,
 	})
 }
