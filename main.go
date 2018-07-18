404358c85d23af37cb2eb7d3d4ffa7e9155050e4
argonavt11 <argonavt.al@yandex.ru>
2018-07-18 20:55:49 +0300

    validate

--- main.go
+++ main.go
@@ -4,70 +4,23 @@ import (
 	"net/http"
 	"log"
 	"github.com/gorilla/mux"
-	"github.com/PuerkitoBio/goquery"
-	"strconv"
+	"./user"
 	"encoding/json"
-	"sync"
 )
 
 type ResultIndex struct {
-	Users []FindUser
+	Connect bool
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	if err := json.NewEncoder(w).Encode(ResultIndex{Connect: true}); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
+	router.HandleFunc("/parse", user.ParseUsers)
 	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	var (
-		wg sync.WaitGroup
-		result []FindUser
-	)
-	for i := 1; i <= 10; i++ {
-		wg.Add(1)
-		go addUsers(i, &wg, &result)
-	}
-	wg.Wait()
-	if err := json.NewEncoder(w).Encode(result); err != nil {
-		panic(err)
-	}
-}
-
-type FindUser struct {
-	Name, UrlProf, Avatar string
-}
-
-func GetParseUsers(url string, c chan []FindUser) {
-	var arrayUser []FindUser
-	res, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	doc.Find(".shout-answer--item").Each(func(i int, s *goquery.Selection) {
-		link, errLink := s.Find(".shout-author").Attr("href")
-		img, errImg := s.Find(".shout-item--pic").Attr("src")
-		if (errLink == true) && (errImg == true) {
-			newUser := FindUser{
-				Name: s.Find(".shout-author--name").Text(),
-				UrlProf: link,
-				Avatar: img,
-			}
-			arrayUser = append(arrayUser, newUser)
-			}
-		})
-	c <- arrayUser
-}
-
-func addUsers(i int, wg *sync.WaitGroup, result *[]FindUser) {
-	c := make(chan []FindUser)
-	go GetParseUsers("https://kanobu.ru/shouts/" + strconv.Itoa(i), c)
-	newUsers := <-c
-	if newUsers != nil {
-		*result = append(*result, newUsers...)
-	}
-	wg.Done()
 }
--- user/user.go
+++ user/user.go
@@ -1,13 +1,66 @@
 package user
 
 import (
-	"github.com/gin-gonic/gin"
-	"../parseUsers"
+	"net/http"
+	"sync"
+	"encoding/json"
+	"log"
+	"github.com/PuerkitoBio/goquery"
+	"strconv"
 )
 
-func GetAll(c *gin.Context) {
-	parseUsers.Start(10)
-	c.JSON(200, gin.H{
-		"users": "not",
+func ParseUsers(w http.ResponseWriter, r *http.Request) {
+	var (
+		wg sync.WaitGroup
+		result []FindUser
+	)
+	for i := 1; i <= 10; i++ {
+		wg.Add(1)
+		go addUsers(i, &wg, &result)
+	}
+	wg.Wait()
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		panic(err)
+	}
+}
+
+type FindUser struct {
+	Name, UrlProf, Avatar string
+}
+
+func GetParseUsers(url string, result *[]FindUser) {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	doc.Find(".shout-answer--item").Each(func(i int, s *goquery.Selection) {
+		link, errLink := s.Find(".shout-author").Attr("href")
+		img, errImg := s.Find(".shout-item--pic").Attr("src")
+		if (errLink == true) && (errImg == true) {
+			newUser := FindUser{
+				Name: s.Find(".shout-author--name").Text(),
+				UrlProf: link,
+				Avatar: img,
+			}
+			if catchReiteration(result, newUser) != false {
+				*result = append(*result, newUser)
+			}
+		}
 	})
+}
+
+func catchReiteration(arrayUser *[]FindUser, user FindUser) bool {
+	for _, v := range *arrayUser {
+		if v.Name == user.Name {
+			return false
+		}
+	}
+	return true
+}
+
+func addUsers(i int, wg *sync.WaitGroup, result *[]FindUser) {
+	GetParseUsers("https://kanobu.ru/shouts/" + strconv.Itoa(i), result)
+	wg.Done()
 }
