c194fa405e320a895c04e3e70968d4d45984a1dd
argonavt11 <argonavt.al@yandex.ru>
2018-09-07 16:03:33 +0300

    create previews for pichers

--- images/images.go
+++ images/images.go
@@ -3,9 +3,9 @@ package images
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
-	_ "image/jpeg"
-	_ "image/png"
+	"image"
+	"image/jpeg"
+	"image/png"
 	"io"
 	"net/http"
 	"os"
@@ -13,8 +13,11 @@ import (
 	"time"
 
 	"../db"
+	"github.com/nfnt/resize"
 	"gopkg.in/mgo.v2/bson"
 )
+
+const WIDTH_PREVIEW = 450
 
 type RequestCreateAlbum struct {
 	Name        string `json:"name" bson:"name"`
@@ -29,11 +32,17 @@ type Album struct {
 }
 
 type Img struct {
-	Name        string        `json:"name" bson:"name"`
-	Time        int64         `json:"time" bson:"time"`
-	AlbumId     bson.ObjectId `json:"albumId" bson:"albumId"`
-	Url         string        `json:"url" bson:"url"`
-	Url_preview string        `json:"urlPreview" bson:"urlPreview"`
+	Name       string        `json:"name" bson:"name"`
+	Time       int64         `json:"time" bson:"time"`
+	AlbumId    bson.ObjectId `json:"albumId" bson:"albumId"`
+	Url        string        `json:"url" bson:"url"`
+	UrlPreview string        `json:"urlPreview" bson:"urlPreview"`
+}
+
+type ImgInfo struct {
+	Width  int
+	Height int
+	Format string
 }
 
 type AlbumId struct {
@@ -102,32 +111,93 @@ func workWithImg(w http.ResponseWriter, r *http.Request, albumId AlbumId) {
 		return
 	}
 	defer file.Close()
-	fmt.Println(handler.Header["Content-Type"][0] == "image/jpeg")
-	if handler.Header["Content-Type"][0] != "image/jpeg" {
+	if handler.Header["Content-Type"][0] != "image/jpeg" && handler.Header["Content-Type"][0] != "image/png" {
 		http.Error(w, "unvalidate format", http.StatusBadRequest)
 		return
 	}
-	fileName := strconv.FormatInt(time.Now().Unix(), 10) + "_" + handler.Filename
+	fileName := strconv.FormatInt(time.Now().UnixNano(), 10) + "_" + handler.Filename
 	fCreate, err := os.Create("./src/img/" + fileName)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	newImg.Name = handler.Filename
-	newImg.Url = fileName
-	newImg.Time = time.Now().Unix()
-	newImg.AlbumId = albumId.ID
+	newImg = Img{Name: handler.Filename, Url: fileName, Time: time.Now().Unix(), AlbumId: albumId.ID}
 	defer fCreate.Close()
 	io.Copy(fCreate, file)
-	// c, _, e := image.DecodeConfig(file)
-	// if e != nil {
-	// 	http.Error(w, e.Error(), http.StatusBadRequest)
-	// 	return
-	// }
+	infoPicher, err := getInfoImg(fileName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if infoPicher.Width < 400 {
+		newImg.UrlPreview = fileName
+		json.NewEncoder(w).Encode(&newImg)
+		return
+	}
+	var preview string
+	if infoPicher.Format == "jpeg" {
+		previewName, err := createPreviewJpeg(fileName)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+		preview = previewName
+	}
+	if infoPicher.Format == "png" {
+		previewName, err := createPreviewPng(fileName)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+		preview = previewName
+	}
+	newImg.UrlPreview = preview
 	json.NewEncoder(w).Encode(&newImg)
-	// if c.Width < 400 {
+}
 
-	// }
+func createPreviewJpeg(fileName string) (string, error) {
+	openImg, err := os.Open("./src/img/" + fileName)
+	if err != nil {
+		return "", err
+	}
+	defer openImg.Close()
+	img, err := jpeg.Decode(openImg)
+	if err != nil {
+		return "", err
+	}
+	m := resize.Resize(WIDTH_PREVIEW, 0, img, resize.Lanczos3)
+	preview, err := os.Create("./src/img/" + "preview_" + fileName)
+	defer preview.Close()
+	jpeg.Encode(preview, m, nil)
+	return "preview_" + fileName, nil
+}
+
+func createPreviewPng(fileName string) (string, error) {
+	openImg, err := os.Open("./src/img/" + fileName)
+	if err != nil {
+		return "", err
+	}
+	defer openImg.Close()
+	img, err := png.Decode(openImg)
+	if err != nil {
+		return "", err
+	}
+	m := resize.Resize(WIDTH_PREVIEW, 0, img, resize.Lanczos3)
+	preview, err := os.Create("./src/img/" + "preview_" + fileName)
+	defer preview.Close()
+	png.Encode(preview, m)
+	return "preview_" + fileName, nil
+}
+
+func getInfoImg(fileName string) (ImgInfo, error) {
+	openImg, err := os.Open("./src/img/" + fileName)
+	if err != nil {
+		return ImgInfo{}, err
+	}
+	defer openImg.Close()
+	infoPicher, format, err := image.DecodeConfig(openImg)
+	if err != nil {
+		return ImgInfo{}, err
+	}
+	return ImgInfo{Width: infoPicher.Width, Height: infoPicher.Height, Format: format}, nil
 }
 
 func foundAlbum(id string, albumId string) (AlbumId, error) {
